04505b14aa02d06b6bc17990db5238bdeabc5f8b
ThisIsNam868 <55947650+ThisisNam868@users.noreply.github.com>
2023-09-24 00:37:03 +0200

    add header

---
+++ src/components/HeaderBar/HeaderBar.js
@@ -0,0 +1,15 @@
+import { Menu } from '../Menu/Menu';
+
+export function HeaderBar() {
+    return (
+        <>
+            <Menu/>
+            <img src="youtube.png" />
+            <img src="search.png"/>
+            <img src="record.png"/>
+            <img src="upload.png"/>
+            <img src="notification.png"/>
+            <img src="profile.png"/>
+        </>
+    );
+}
